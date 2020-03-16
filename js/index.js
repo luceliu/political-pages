66ac7984b6b37ea80e490e88df9422ee62d5940f
Sherry Xie <sherryxy.71@gmail.com>
2020-03-15 17:40:13 -0700

    update data with engagement_count column

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// TO DO
+// Load data
+Promise.all([
+    d3.csv('/data/facebook-fact-check.csv'),
+  ]).then(files => {
+    let data = files[0];
+
+    // Change all engagement counts to numbers
+    data.forEach(d => {
+      const columns = Object.keys(d)
+      for (const col of columns) {
+        if (col == "share_count" || col == "reaction_count" || col == "comment_count" || col == "engagement_count") {
+          d[col] = +d[col];
+        }
+      }
+    });
+  
+    // Initialize bubble vis
+    let postBubbles = new bubbleVis({ 
+      parentElement: '#bubbleVis',
+      colorValue: d => d[Rating],
+      pageValue: d => d[Page],
+      selectedCategory: 'left'
+    });
+  
+    postBubbles.render();  
+    
+  });
+  
