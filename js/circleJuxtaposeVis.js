3ace98af959cb623d3af96c592738b024639b5e7
Amy Zhu <amyjzhu@thetis.students.cs.ubc.ca>
2020-03-21 14:11:10 -0700

    Slight reorganize

---
+++ js/circleJuxtaposeVis.js
@@ -0,0 +1,38 @@
+class circleJuxtaposeVis {
+    
+    constructor(_config) {
+        this.config = {
+          parentElement: _config.parentElement,
+          containerWidth: _config.containerWidth || 1000,
+          containerHeight: _config.containerHeight || 870,
+        }
+    
+        this.data = _config.data;
+        // this.pageValue = _config.pageValue;
+        // this.idValue = _config.idValue;
+        // this.colorValue = _config.colorValue; 
+        // this.zValue = _config.zValue;
+        // this.selectedCategory = _config.selectedCategory;
+        // this.svg = d3.select(this.config.parentElement);
+        // this.chart = this.svg.append('g');
+        // this.formatValue = _config.formatValue;
+        // this.linkValue = _config.linkValue;
+    
+        this.initVis();
+      }
+
+
+    initVis() {
+
+
+    }
+
+    update() {
+
+
+    }
+
+    render() {
+        
+    }
+}
