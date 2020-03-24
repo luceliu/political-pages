6a589fe8490a4ccf90594be3f77d0d9a31bafdeb
Sherry Xie <sherryxy.71@gmail.com>
2020-03-24 01:44:12 -0700

    update layout

--- js/stackedBarVis.js
+++ js/stackedBarVis.js
@@ -2,8 +2,8 @@ class stackedBarVis {
     constructor(_config) {
         this.config = {
           parentElement: _config.parentElement,
-          containerWidth: _config.containerWidth || 1000,
-          containerHeight: _config.containerHeight || 870,
+          containerWidth: _config.containerWidth || 500,
+          containerHeight: _config.containerHeight || 470,
         }
         this.config.margin = _config.margin || { top: 40, bottom: 40, right: 0, left: 0 }
         this.data = _config.data;
@@ -27,7 +27,6 @@ class stackedBarVis {
             .attr('class', 'vis-title')
             .attr('y', -10)
             .attr('x', 0)
-            .text('Percentage of a page’s total posts by truthfulness rating')
 
           const formatter = d3.format(".0%");
 
@@ -44,7 +43,7 @@ class stackedBarVis {
           vis.yScale = d3.scaleBand()
             .domain(pageTitles)
             .range([0, vis.height])
-            .padding(0.2);
+            .padding(0.3);
 
           console.log('test: ', vis.yScale("Eagle Rising")) // not giving a valid y value??
 
@@ -114,7 +113,7 @@ class stackedBarVis {
                 .enter()
                 .append("rect")
                 .attr("width", d => vis.widthScale(d[1] - d[0]))
-                .attr("height", 40)
+                .attr("height", 28)
                 .attr('y', (p, i) => vis.yScale(pageTitles[i])) // giving completely bogus y values???
                 .attr('x', p => vis.xScale(p[0])) // changed from p[1] to p[0]
                 .style("fill", vis.colorScale(d))
