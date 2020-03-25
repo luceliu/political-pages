fe4647b37e76bdf73d762d0d9a3c79bc2fdd85d5
Amy Zhu <ajyzhu@gmail.com>
2020-03-24 20:39:57 -0700

    Convert circles to percentage

--- js/circleJuxtaposeVis.js
+++ js/circleJuxtaposeVis.js
@@ -13,7 +13,9 @@ class circleJuxtaposeVis {
         this.config.margin = _config.margin || { top: 40, bottom: 20, right: 0, left: 0 }
 
         this.data = _config.data;
-        this.postMap = _config.postMap;
+        // deep copy this object so I can mess with it
+        this.postMap = JSON.parse(JSON.stringify(_config.postMap));
+
         this.duration = _config.duration || 700;
 
         this.onMouseover = _config.onMouseover;
@@ -43,13 +45,24 @@ class circleJuxtaposeVis {
         // create two kinds of scales 
         // one for placement of total posts and one for fake
         // and one y-scale for the number of sites (9)
+
+        // TODO compute percentages
+        let ratingArray = ["mostly false", "mostly true", "mixture of true and false",
+            "no factual content"];
+
+        vis.postMap.forEach(
+            d => ratingArray.forEach(r =>
+                d[r] = d[r] / d.total)
+        )
+        console.log(vis.postMap)
+
         vis.xScale = d3.scaleBand()
             .domain([0, 1, 2])
             .range([0, vis.width - vis.frontTextPadding + vis.backTextPadding])
             .padding(0.2);
 
-        let arrayOfObjSummary = (arr, funcArray, funcObj) =>
-            funcArray(...arr.map(d => funcObj(...Object.values(d).filter(a => !isNaN(a)))));
+        let computeStatistic = (func) =>
+            func(...vis.postMap.map(d => func(...ratingArray.map(r => d[r]))));
 
         vis.yScale = d3.scaleBand()
             .domain(vis.postMap.map(d => d.name))
@@ -58,8 +71,8 @@ class circleJuxtaposeVis {
 
         // stand-in for now (domain across all values)
         vis.postRadiusScale = d3.scaleSqrt()
-            .domain([arrayOfObjSummary(vis.postMap, Math.min, Math.min),
-            arrayOfObjSummary(vis.postMap, Math.max, Math.max)])
+            .domain([computeStatistic(Math.min),
+            computeStatistic(Math.max)])
             .range([5, 30]);
 
         vis.totalRadiusScale = d3.scaleSqrt()
@@ -67,35 +80,38 @@ class circleJuxtaposeVis {
             Math.max(...vis.postMap.map(d => d.total))])
             .range([10, 40]);
 
-            // TODO - get this from somewhere else
+        console.log(vis.postRadiusScale.domain())
+        console.log(vis.totalRadiusScale.domain())
+
+        // TODO - get this from somewhere else
         vis.colourScale = d3.scaleOrdinal()
-        .domain(['no factual content', 'mostly false', 'mixture of true and false', 'mostly true'])
-        .range(['#634265', '#E05E5E', '#D3DCE7', '#67D99B']);
+            .domain(['no factual content', 'mostly false', 'mixture of true and false', 'mostly true'])
+            .range(['#634265', '#E05E5E', '#D3DCE7', '#67D99B']);
 
         vis.sortKey = "mostly false";
 
         vis.totalkey = vis.chart.append("text");
         vis.totalkey
-        .attr('class', 'key-text')
-        .attr('x', 930) // todo another magic number...
-        .attr('y', 0)
-        .text("Total posts");
+            .attr('class', 'key-text')
+            .attr('x', 930) // todo another magic number...
+            .attr('y', 0)
+            .text("Total posts");
 
         vis.sortKeyPre = vis.chart.append("text");
         vis.sortKeyPre
-        .attr('class', 'key-text')
-        .text("Posts rated as")
-        .attr('x', 0)
-        .attr('y', 0)
+            .attr('class', 'key-text')
+            .text("Percentage posts rated")
+            .attr('x', 0)
+            .attr('y', 0)
 
         vis.sortKeyText = vis.chart.append("text");
         vis.sortKeyText
-        .attr('class', 'key-text')
-        .text(vis.sortKey)
-        .attr('x', 0)
-        // TODO lots of magic numbers related to pixels
-        .attr('y', 20)
-        
+            .attr('class', 'key-text')
+            .text(vis.sortKey)
+            .attr('x', 0)
+            // TODO lots of magic numbers related to pixels
+            .attr('y', 20)
+
         vis.group = vis.chart.append('g');
         vis.group.attr('transform', `translate(${vis.frontTextPadding}, 0)`);
 
@@ -107,8 +123,8 @@ class circleJuxtaposeVis {
         let vis = this;
 
         vis.sortKeyText
-        .transition()
-        .text(vis.sortKey);
+            .transition()
+            .text(vis.sortKey);
 
         vis.postMap = vis.postMap.sort((a, b) => b[vis.sortKey] - a[vis.sortKey]);
 
@@ -126,7 +142,7 @@ class circleJuxtaposeVis {
         // set up groups
         let vis = this;
 
-        let id = function(d) { return d.name; }
+        let id = function (d) { return d.name; }
         let groups = vis.group.selectAll('g')
             .data(vis.postMap, id);
 
@@ -154,10 +170,10 @@ class circleJuxtaposeVis {
                 d3.select(this)
                     .attr('transform', `translate(${-d.textWidth / 2}, 0)`)
             });
-            // TODO janky way of setting the placement of text
-            //.each(function(d) { d.nameWidth = this.getBBox().width })
-            //.attr('transform', function(d) { `translate(${-d.nameWidth / 2}, 0)` });
-            
+        // TODO janky way of setting the placement of text
+        //.each(function(d) { d.nameWidth = this.getBBox().width })
+        //.attr('transform', function(d) { `translate(${-d.nameWidth / 2}, 0)` });
+
 
         let line = groups.selectAll('.line').data(d => [d]);
         line.enter()
@@ -183,7 +199,7 @@ class circleJuxtaposeVis {
             .transition().duration(vis.duration)
             .attr('fill', d => vis.colourScale(vis.sortKey))
             .attr('cx', vis.xScale(0))
-            .attr('r', d => vis.postRadiusScale(d[vis.sortKey]) + circleStroke /2)
+            .attr('r', d => vis.postRadiusScale(d[vis.sortKey]) + circleStroke / 2)
             // TODO in-progress: this is very janky; there must be a better way to get value
             .attr('cy', function (d) {
                 // d.postCirclePosY = vis.yScale(d.name)
@@ -201,7 +217,7 @@ class circleJuxtaposeVis {
             .merge(totalCircle)
             .transition().duration(vis.duration)
             .attr('cx', vis.xScale(2))
-            .attr('r', d => vis.totalRadiusScale(d.total) + circleStroke /2)
+            .attr('r', d => vis.totalRadiusScale(d.total) + circleStroke / 2)
             .attr('cy', function (d) {
                 // d.totalCirclePosY = vis.yScale(d.name)
                 //     - (vis.totalRadiusScale(d.total) / 2);
@@ -228,7 +244,7 @@ class circleJuxtaposeVis {
             .attr('x', d => vis.xScale(1) - d.textWidth / 2 - (d.textWidth * 0.2 / 2))
             .attr('fill', d => (d.name === vis.selectedPage && vis.selectedPage != null) ? '#d3d3d3' : 'white')
 
-        groups.selectAll('.name-label').each(function(d) {
+        groups.selectAll('.name-label').each(function (d) {
             d3.select(this).raise();
         });
 
